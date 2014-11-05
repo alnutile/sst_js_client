01f97443fe27d753632d3c303a7571886ca40191
Andrew <cavanaghacea@gmail.com>
2014-11-05 10:09:47 -0500

    Delete sst_js_client.js

--- js/sst_js_client.js
+++
@@ -1,6 +0,0 @@
-jQuery.ajax({
-    url: "https://smscvt.atlassian.net/s/d41d8cd98f00b204e9800998ecf8427e/en_US34kp20-1988229788/6204/207/1.4.0-m8/_/download/batch/com.atlassian.jira.collector.plugin.jira-issue-collector-plugin:issuecollector-embededjs/com.atlassian.jira.collector.plugin.jira-issue-collector-plugin:issuecollector-embededjs.js?collectorId=c7023ed3",
-    type: "get",
-    cache: true,
-    dataType: "script"
-});
